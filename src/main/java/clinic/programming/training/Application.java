6c708863229b5ec33cf8e217d935a7d925131a73
Neto Meira <jose.meira.neto@gmail.com>
2018-09-21 00:03:28 -0300

    Updating gitignore and adding Java source

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,15 +1,28 @@
 
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
 
+    public void doIt(){
+        List<String> list = new ArrayList<>();
+        list.add("Greetings");
+
+        for (String elemento : list){
+            System.out.println(elemento);
+        }
+    }
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	    Application app = new Application();
+        Application app = new Application();
+        app.doIt();
     }
 }
